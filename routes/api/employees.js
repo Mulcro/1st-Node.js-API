f0a616082c7fca2a8fbd437f006e0512124cdb6c
Mulero Alamou <Mulero.alamou@gmail.com>
2022-10-07 21:25:20 +0100

    added logout route

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const employeeController = require('../../controllers/employeesControler');
+const ROLES_LIST = require('../../config/roles');
+const verifyRoles = require('../../middleware/verifyRole');
 
 router.route('/')
     .get(employeeController.getAllEmployees)
-    .post(employeeController.createNewEmployee)
+    .post(verifyRoles(ROLES_LIST.Admin,ROLES_LIST.Editor),employeeController.createNewEmployee)
 
 router.route('/:id')
-    .get(employeeController.getEmployee)
-    .put(employeeController.updateEmployee)
-    .delete(employeeController.deleteEmployee);
+    .get(verifyRoles(ROLES_LIST.Admin,ROLES_LIST.Editor),employeeController.getEmployee)
+    .put(verifyRoles(ROLES_LIST.Admin,ROLES_LIST.Editor),employeeController.updateEmployee)
+    .delete(verifyRoles(ROLES_LIST.Admin),employeeController.deleteEmployee);
 
 module.exports = router;
