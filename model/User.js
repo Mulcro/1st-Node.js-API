016f8f7493c2fa4f23de623b1c4d5b7e91d918f1
Mulero Alamou <Mulero.alamou@gmail.com>
2022-10-07 21:24:50 +0100

    deleted hardcoded json and configured database models

---
+++ model/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username:{
+        type: String,
+        required: true
+    },
+    roles:{
+        User: {
+            type: Number,
+            default: 1000
+        },
+        Editor: Number,
+        Admin: Number
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    refreshToken: {
+        type: String,
+        default: ""
+    }
+});
+
+module.exports = mongoose.model('User',userSchema);
